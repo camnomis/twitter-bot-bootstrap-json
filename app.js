d21e940483d81494cd6774e5a49ebaa8f65ccde9
Simon McCartney <simon@camnomis.com>
2020-02-19 14:14:53 +0000

    Changed variables being read from json

--- app.js
+++ app.js
@@ -26,10 +26,10 @@ https.get('https://ws.audioscrobbler.com/2.0/?method=user.getLovedTracks&user=zh
   resp.on('end', () => {
     // *** This stuff has been moved inside this callback function, so it can "see" the data ***
     var response_object = JSON.parse(data);
-    var Artist = response_object.track.artist.name;
-    var Track = response_object.track.name;
-    var Image = response_object.track.image.small.text;
-    var URL = response_object.track.url;
+    var Artist = response_object.artist.name;
+    var Track = response_object.name;
+    var Image = response_object.image.small.text;
+    var URL = response_object.url;
 
     Twitter.post('statuses/update', { status: 'New loved track'+Artist+' '+Track }, function(err, data, response) {
       if(err){
